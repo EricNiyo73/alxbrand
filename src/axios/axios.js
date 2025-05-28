fc3a3586074652647affff24720b8c290d9b7444
EricNiyo73 <ericniyokwizerwa1@gmail.com>
2025-05-28 22:56:39 +0200

    remove loacalhost

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 //url
 export default axios.create({
-  baseURL: "https://alxbackend-1.onrender.com/",
+  baseURL: "https://alxbackend-1.onrender.com/api/v1/",
 });
 
 // export default axios.create({
